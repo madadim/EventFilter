f24b9facfe3cd4e7fe02d9c8e1940c9a04ff6630
madadim <madadim@gmail.com>
2017-05-05 15:42:41 -0400

    version change

--- Resources/HiveReadObject.sh
+++ Resources/HiveReadObject.sh
@@ -1,8 +1,14 @@
-CLASS_PATH_COLON=/usr/hdp/current/hive-client/lib/*.jar
+#!/bin/bash
+export SPARK_HOME=/usr/hdp/current/spark-client
+export LIB_DIR=/u02/jars
+
+#grab jars
+libs=`ls -m $LIB_DIR/*.jar | tr -d ' ' | tr -d '\n'`
 
 /usr/hdp/current/spark-client/bin/spark-submit \
---class com.first.scala.HiveReadObject \
---master yarn-cluster \
+--jars ${libs} \
+--conf spark.dynamicAllocation.enabled=true \
 --files /usr/hdp/current/spark-client/conf/hive-site.xml,/usr/hdp/current/hadoop-client/conf/core-site.xml,/usr/hdp/current/hadoop-client/conf/hdfs-site.xml \
---driver-class-path $CLASS_PATH_COLON \
+--class com.first.scala.HiveReadObject \
 /u02/code/EventFilters-0.0.1-SNAPSHOT.jar
+
